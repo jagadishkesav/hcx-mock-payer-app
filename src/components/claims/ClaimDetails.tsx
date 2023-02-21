d64f77f49042c2c6c035b0a495b2693c5ac3bc76
rithviknishad <rithvikn2001@gmail.com>
2023-02-21 19:23:36 +0530

    disable approve reject buttons conditionally

--- src/components/claims/ClaimDetails.tsx
+++ src/components/claims/ClaimDetails.tsx
@@ -46,6 +46,7 @@ export function FinancialInfo({
   claim,
   ...props
 }: { claim: ClaimDetail } & RejectApproveHandlers) {
+  const medical_info = claim.medical_info;
   const financial_info = claim.financial_info;
   const [approvedAmount, setApprovedAmount] = React.useState(
     parseFloat(
@@ -122,34 +123,37 @@ export function FinancialInfo({
         </div>
       </dl>
 
-      {claim.financial_info.status === "Pending" && (
-        <div className="flex flex-row justify-end w-full space-x-4 p-5">
-          <button
-            onClick={() =>
-              props.handleReject({
-                request_id: claim.request_id,
-                type: "financial",
-              })
-            }
-            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-          >
-            Reject
-          </button>
-          <button
-            onClick={() =>
-              props.handleApprove({
-                request_id: claim.request_id,
-                type: "financial",
-                approved_amount: approvedAmount,
-                remarks,
-              })
-            }
-            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-          >
-            Approve
-          </button>
-        </div>
-      )}
+      {claim.financial_info.status === "Pending" &&
+        claim.status === "Pending" && (
+          <div className="flex flex-row justify-end w-full space-x-4 p-5">
+            <button
+              disabled={medical_info.status !== "Approved"}
+              onClick={() =>
+                props.handleReject({
+                  request_id: claim.request_id,
+                  type: "financial",
+                })
+              }
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+            >
+              Reject
+            </button>
+            <button
+              disabled={medical_info.status !== "Approved"}
+              onClick={() =>
+                props.handleApprove({
+                  request_id: claim.request_id,
+                  type: "financial",
+                  approved_amount: approvedAmount,
+                  remarks,
+                })
+              }
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
+              Approve
+            </button>
+          </div>
+        )}
     </div>
   );
 }
@@ -214,34 +218,35 @@ export function MedicalInfo({
           </dd>
         </div>
       </dl>
-      {claim.medical_info.status === "Pending" && (
-        <div className="flex flex-row justify-end w-full space-x-4 p-5">
-          <button
-            onClick={() =>
-              props.handleReject({
-                request_id: claim.request_id,
-                type: "medical",
-              })
-            }
-            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-          >
-            Reject
-          </button>
-          <button
-            onClick={() =>
-              props.handleApprove({
-                request_id: claim.request_id,
-                type: "medical",
-                approved_amount: approvedAmount,
-                remarks,
-              })
-            }
-            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-          >
-            Approve
-          </button>
-        </div>
-      )}
+      {claim.medical_info.status === "Pending" &&
+        claim.status === "Pending" && (
+          <div className="flex flex-row justify-end w-full space-x-4 p-5">
+            <button
+              onClick={() =>
+                props.handleReject({
+                  request_id: claim.request_id,
+                  type: "medical",
+                })
+              }
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+            >
+              Reject
+            </button>
+            <button
+              onClick={() =>
+                props.handleApprove({
+                  request_id: claim.request_id,
+                  type: "medical",
+                  approved_amount: approvedAmount,
+                  remarks,
+                })
+              }
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
+              Approve
+            </button>
+          </div>
+        )}
     </div>
   );
 }
@@ -345,10 +350,7 @@ export function PatientDetails({ claim }: { claim: any }) {
 
 const handleReject = ({ request_id, type }: any) => {
   rejectClaim({ request_id, type });
-  toast("Claim Rejected", {
-    type: "success",
-  });
-  navigate("/claims");
+  toast("Claim Rejected", { type: "success" });
 };
 
 const handleApprove = ({ request_id, type, remarks, approved_amount }: any) => {
@@ -358,10 +360,7 @@ const handleApprove = ({ request_id, type, remarks, approved_amount }: any) => {
     remarks,
     approved_amount,
   });
-  toast("Claim Approved", {
-    type: "success",
-  });
-  navigate("/claims");
+  toast("Claim Approved", { type: "success" });
 };
 
 export default function ClaimDetails({ request_id }: { request_id: string }) {
